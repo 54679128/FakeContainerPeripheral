f5e66ebf06b774a37408ca35625c9deb94f34dac
54679128 <a54679128@outlook.com>
2026-04-17 15:47:08 +0800

    feat(peripheral): 添加模块内使用的外设存在检查函数

--- src/peripheral.lua
+++ src/peripheral.lua
@@ -3,6 +3,12 @@ local localNet = require("localNet")
 -- 模拟 `peripheral` API，将被注入全局环境中
 local out = {}
 
+local function assertExist(name)
+    if not localNet.isPresent(name) then
+        error(("Can't find peripheral: %s"):format(name), 3)
+    end
+end
+
 function out.isPresent(name)
     return localNet.isPresent(name)
 end
