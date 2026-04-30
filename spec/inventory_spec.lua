d367df941d24416a2c47ac29fe26139616b7555a
54679128 <a54679128@outlook.com>
2026-04-30 11:13:59 +0800

    test(inventory_spec): 添加对`inventory`模块提供的`FakeItem`创建函数测试

--- spec/inventory_spec.lua
+++ spec/inventory_spec.lua
@@ -1,3 +1,19 @@
-describe("物品容器组件（inventory）模块测试",function ()
-    
+local inventory = require("ContainerComponent.inventory")
+
+local function makeStone(count)
+    return inventory.FakeItem.make("minecraft:stone", count or 1, 64, {})
+end
+
+local function makeSimpleSword(count)
+    return inventory.FakeItem.make("a546:simple_sword", count or 1, 1, { damage = 0 })
+end
+
+describe("物品容器组件（inventory）模块测试", function()
+    it("FakeItem创建函数测试", function()
+        local grass = inventory.FakeItem.make("minecraft:grass", 22, 64, {})
+        assert.Equal(grass.name, "minecraft:grass")
+        assert.Equal(grass.count, 22)
+        assert.Equal(grass.stackLimit, 64)
+        assert.Same(grass.nbt, {})
+    end)
 end)
